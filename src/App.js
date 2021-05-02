90ed7acbab69efd955f8a90b96c95cdb147fb890
AbdulHakim96 <abdulhaakim96@gmail.com>
2021-05-02 23:16:52 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ const [people, setPeople] = useState(data)
   return (
       <main>
           <section className = 'container' >
-          <h3>{people.length} birthdays today </h3>
+          <h3>{people.length} Birthdays Today </h3>
     <List people ={people} />
-    <button onClick={()=>setPeople([])}>clear All </button>
+    <button onClick={()=>setPeople([])}>Clear All </button>
     </section>
     </main>
 
